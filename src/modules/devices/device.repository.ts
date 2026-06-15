ee3ce7f9cd8bc0621d9fe7464dfbc7d7a0befa67
Igor Lima <igorlimadeveloper@gmail.com>
2026-06-15 07:24:07 -0300

    feat: add update and getById methods to device service for improved functionality

--- src/modules/devices/device.repository.ts
+++ src/modules/devices/device.repository.ts
@@ -5,6 +5,8 @@ import { ID } from "@/types";
 export const deviceRepository = {
   findAll: () => prisma.device.findMany(),
   findById: (id: ID) => prisma.device.findUnique({ where: { id } }),
+  update: (id: ID, data: CreateDevice) =>
+    prisma.device.update({ where: { id }, data }),
   create: (data: CreateDevice) => prisma.device.create({ data }),
   delete: (id: ID) => prisma.device.delete({ where: { id } }),
 };
--- src/modules/devices/device.service.ts
+++ src/modules/devices/device.service.ts
@@ -6,13 +6,17 @@ import { NotFoundError } from "@/common/helpers/error";
 export const deviceService = {
   getAll: () => deviceRepository.findAll(),
 
+  getById: (id: ID) => deviceRepository.findById(id),
+
   create: (data: CreateDevice) => deviceRepository.create(data),
 
-  delete: async (id:ID) => {
-    const device = await deviceRepository.findById(id)
+  update: (id: ID, data: CreateDevice) => deviceRepository.update(id, data),
 
-    if (!device) throw new NotFoundError('Dispositivo não encontrado');
+  delete: async (id: ID) => {
+    const device = await deviceRepository.findById(id);
 
-    return deviceRepository.delete(id)
-  }
+    if (!device) throw new NotFoundError("Dispositivo não encontrado");
+
+    return deviceRepository.delete(id);
+  },
 };
