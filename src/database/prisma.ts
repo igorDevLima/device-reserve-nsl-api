1196805cfa94a769a711ca5907b632382ab47da3
Igor Lima <igorlimadeveloper@gmail.com>
2026-06-19 10:31:46 -0300

    chore: remove redundant URL definition in schema.prisma and ensure dbPath is used in prisma.ts

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -17,6 +17,7 @@ export const prisma =
   new PrismaClient({
     adapter,
     log: ["query"],
+    accelerateUrl: dbPath
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
