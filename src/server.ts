691ccfecce48da8e68b1e986b7a1671afd01b1aa
Igor Lima <igorlimadeveloper@gmail.com>
2026-06-19 10:00:15 -0300

    chore: update server.ts to listen on fixed port and log simplified message

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import { app } from './app'
 
-const PORT = process.env.PORT ?? 3000
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`)
-})
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Servidor rodando...');
+});
